75787ffdcccbd7cea226743e3e4b4af62f83e283
razaZainikLab <rakibraza.zainiklab@gmail.com>
2022-04-08 14:21:59 +0600

    gallery section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,83 @@
         }
       ]
     });
+    /*-------------------------------------------
+    story-box-slide active
+    --------------------------------------------- */
+    $('.story-box-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: true,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    /*-------------------------------------------
+    blog-slide active
+    --------------------------------------------- */
+    $('.blog-slide').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: true,
+      prevArrow: '<i class="slick-prev fas fa-angle-left"></i> ',
+      nextArrow: '<i class="slick-next fas fa-angle-right"></i> ',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
   })
   // Kursor
   // new kursor({
